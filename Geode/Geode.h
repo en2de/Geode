c6ef7a3d89d0c6dec99d077963af7b701847713a
envy.chen <envx.chen@gmail.com>
2016-09-30 12:03:55 +0800

    initial commit

---
+++ Geode/Geode.h
@@ -0,0 +1,19 @@
+//
+//  Geode.h
+//  Geode
+//
+//  Created by envy.chen on 30/09/2016.
+//  Copyright © 2016 en2de.github.io. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Geode.
+FOUNDATION_EXPORT double GeodeVersionNumber;
+
+//! Project version string for Geode.
+FOUNDATION_EXPORT const unsigned char GeodeVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Geode/PublicHeader.h>
+
+
