8e2fcc931aa03185ba8edb178a42c8d3ebb87073
envy.chen <envx.chen@gmail.com>
2016-10-02 10:52:23 +0800

    add Then protocol

---
+++ Geode/Protocols/Then.swift
@@ -0,0 +1,21 @@
+//
+//  Then.swift
+//  Geode
+//
+//  Created by envy.chen on 02/10/2016.
+//  Copyright © 2016 en2de.github.io. All rights reserved.
+//
+
+import Foundation
+
+public protocol Then {
+}
+
+extension Then {
+    public func then(block: (Self) -> Void) -> Self {
+        block(self)
+        return self
+    }
+}
+
+extension NSObject: Then {}
